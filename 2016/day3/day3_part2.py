f7077fd7f9d07af6f3956006ec572f7917e39e51
Julio Angulo <solojoas@gmail.com>
2024-03-16 13:28:38 +0100

    Year 2016 day3 part2 solved!

--- 2016/day3/day3_part2.py
+++ 2016/day3/day3_part2.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     sidesc = []
 
     with open(filepath) as f:
-        triangles = 0
+        
         for l in f.readlines():
             a, b, c = [int(d) for d in l.strip().split()]
             sidesa.append(a)
@@ -16,3 +16,11 @@ if __name__ == "__main__":
             sidesc.append(c)
     
     sides = sidesa + sidesb + sidesc
+    triangles = 0
+    for i in range(0, len(sides), 3):
+        a, b, c = sides[i:i+3]
+        if istriangle(a, b, c):
+                print(f"found a triangle with sides {(a, b, c)}")
+                triangles += 1
+    
+    print(f"total triangles found: {triangles}")
