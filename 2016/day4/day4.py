be3a9676f72b861da90e0de003833ec9c889502f
Julio Angulo <solojoas@gmail.com>
2024-03-17 08:18:01 +0100

    2016 day 4 part 2 finished
    Started to log status in README file

--- 2016/day4/day4.py
+++ 2016/day4/day4.py
@@ -1,4 +1,6 @@
 import collections
+import string
+import re
 
 def checksum(words):
     letter_freq = calculate_letter_freq(words)
@@ -23,6 +25,25 @@ def get_encrypted_names():
         for line in f.readlines():
             yield line.strip()
 
+################## FUCNTIONS FOR PART 2
+
+def decrypt(msg: str, sector_id: int)->str:
+
+    decoded = map( lambda c : decode_char(c, sector_id), msg)
+    return "".join(decoded)
+
+def decode_char(c: str, sector_id: int)->str:
+    if c == "-":
+        return " "
+    ALFABET = string.ascii_lowercase
+    alfabet_size = len(ALFABET)
+    return ALFABET[(char_pos(c) + sector_id) % alfabet_size]
+
+
+def char_pos(c: str)->int:
+    return ord(c) - ord("a")
+
+###################################33
 if __name__ == "__main__":
     # examples:
     encrypted_names = [
@@ -40,12 +61,15 @@ if __name__ == "__main__":
         words = matches[:-1]
         sector_id_and_checksum = matches[-1]
         sector_id, curr_checksum = sector_id_and_checksum.split("[")
+        sector_id = int(sector_id)
         curr_checksum = curr_checksum.strip("]")
         calculated_checksum = checksum(words)
 
         if calculated_checksum == curr_checksum:
             print(f"{name} is a real room")
-            real_rooms_sum_ids += int(sector_id)
+            real_rooms_sum_ids += sector_id
+            room_name = "-".join(words)
+            print(f"{room_name} ===> {decrypt(room_name, sector_id)}")
             print("=================")
 
     print(f"Sum of sector IDs of the real rooms = {real_rooms_sum_ids}")
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # AdventOfCode
 Repo to save Advent Of Code solutions
+
+## 2015
+## 2016
+- day 1
+    - [ ] [part 1](2016/day1/day1.py)
+    - [ ] part 2
+
+- day 4
+    - [x] [part 1](2016/day4/day4.py)
+    - [x] [part 2](2016/day4/day4.py)
