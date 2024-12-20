28ab5e64d569db6d9ad79dfaadf791d444df7522
Julio Angulo <solojoas@gmail.com>
2024-12-20 12:11:40 -0600

    Improved template creator script to support python

--- aocLib/tools/make_day_template.py
+++ aocLib/tools/make_day_template.py
@@ -3,28 +3,31 @@ import argparse
 from pathlib import Path
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="utility to setup AoC C++ files")
+    parser = argparse.ArgumentParser(description="utility to setup AoC Startup files")
     parser.add_argument("year")
     parser.add_argument("day")
     parser.add_argument("directory")
+    parser.add_argument("--lang",  default="cpp")
 
     args = parser.parse_args()
 
     year = args.year
     day = args.day
     path = args.directory
-
+    lang = args.lang
+    
     print(f"configuring folder {path}/{year}/{day} ...")
 
     path = f"{path}/{year}/day{day}"
-    templates_path = Path.cwd()/"templates"
-    
-    # TODO replace aoc20XXDayXX in cmake by teh year an day
+    templates_path = Path.cwd()/f"templates/{lang}"
 
     cmds = [
         f"mkdir -p {path}",
-        f"mkdir -p {path}/build"
     ]
+
+    if lang == "cpp":
+        cmds.append(f"mkdir -p {path}/build")
+        # TODO replace aoc20XXDayXX in cmake by teh year an day
 
     for cmd in cmds:
         subprocess.run(cmd.split())
---
+++ aocLib/tools/templates/python/solution.py
@@ -0,0 +1,8 @@
+def main():
+    input_file = "input.txt" # input_test.txt
+
+    with open(input_file) as input_data:
+        ...
+
+if __name__ == "__main__":
+    main()
