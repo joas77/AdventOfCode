8ca21849ed96c431a9d3d8bd00113a3163880994
Julio Angulo <solojoas@gmail.com>
2024-12-19 20:59:24 -0600

    year 2024 day 3 part 2

--- events/2024/day3/day3.py
+++ events/2024/day3/day3.py
@@ -6,13 +6,23 @@ def solve_mult_inst(instrucion):
 
 if __name__ == "__main__":
     input_file = "input.txt"
-
+    is_part2 = True
     with open(input_file) as input_data:
         memory = input_data.read()
         
         # instruction mul(X,Y), X, Y are 1-3 digits numbers
-        instruction_pattern = re.compile(r"mul\(\d{1,3},\d{1,3}\)")
+        instruction_pattern = re.compile(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)")
 
         instructions = instruction_pattern.findall(memory)
-        result = sum(solve_mult_inst(ins) for ins in instructions)
+        enabled = True
+        result = 0
+        for ins in instructions:
+            if "mul" in ins and enabled:
+                result += solve_mult_inst(ins)
+            elif is_part2 and "don't" in ins:
+                enabled = False
+            elif is_part2 and ins == "do()":
+                enabled = True
+
         print(result)
+        
